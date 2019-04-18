c4d538b1bfe19734c409c82b692407a008b99f3f
yzjlb <2541012655@qq.com>
2019-04-18 17:31:14 +0800

    ...

---
+++ gobang-gdx-game/src/com/xl/gobang/view/ChessBoardView.java
@@ -0,0 +1,37 @@
+package com.xl.gobang.view;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+/**
+ * 棋盘
+ * @author Administrator
+ *
+ */
+public class ChessBoardView extends Actor{
+	
+	@Override
+	public void setPosition(float x, float y) {
+		// TODO Auto-generated method stub
+		super.setPosition(x, y);
+	}
+	
+	@Override
+	public void draw(Batch batch, float parentAlpha) {
+		// TODO Auto-generated method stub
+		super.draw(batch, parentAlpha);
+	}
+	
+	@Override
+	public void act(float delta) {
+		// TODO Auto-generated method stub
+		super.act(delta);
+	}
+	
+	//落子
+	
+	public void name() {
+		
+	}
+
+}
